88e25e389f8159bff3a684a932584bf5290ad88b
deploy <deploy@users.noreply.github.com>
2022-10-27 05:42:52 +0000

    Updated Alpha alpha-pr6

--- alpha-pr6/components/SideNavigation.js
+++ alpha-pr6/components/SideNavigation.js
@@ -1735,46 +1735,73 @@ var require_emotion_css_cjs = __commonJS({
   }
 });
 
-// src/components/Button.tsx
+// src/components/SideNavigation.tsx
 var import_css = __toModule(require_emotion_css_cjs());
 import {
   createElement
 } from "react";
 import { tokens } from "../tokens.js";
-function Button({ text, link, size, type }) {
-  return /* @__PURE__ */ createElement("button", {
-    type: "button",
-    className: import_css.css`
-        padding: ${size === "normal" ? "8px 24px 8px 24px" : "4px 24px 4px 24px"};
-        border-radius: 8px;
-        background-color: ${type === "primary" ? tokens.color.main_green_10 : tokens.color.grey_10_light};
-        border: 1px solid ${tokens.color.grey_30_light};
-        color: ${type === "primary" ? tokens.color.grey_5_light : tokens.color.grey_50_light};
-        font-weight: 500;
-        font-size: 16px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        cursor: pointer;
-        white-space: nowrap;
-        vertical-align: baseline;
-        line-height: 1.875;
-
-        font-family: 'Pretendard Variable', Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
-          'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji',
-          'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
-
-        @font-face {
-          font-family: 'Pretendard Variable', Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
-            'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji',
-            'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
-          src: url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable.css');
-        }
-      `
-  }, text);
+import { useState } from "react";
+function SideNavigation({ theme, hasIcon, list, focus }) {
+  const [onFocus, setOnFocus] = useState(focus);
+  const handleFocus = (index) => {
+    setOnFocus(index);
+  };
+  return /* @__PURE__ */ createElement("div", {
+    className: contaier
+  }, list.map((list2, index) => {
+    return /* @__PURE__ */ createElement("div", {
+      className: itemContainer,
+      onClick: () => handleFocus(index)
+    }, /* @__PURE__ */ createElement("a", {
+      className: import_css.css`
+                font-size: 16px;
+                color: ${focus === index ? tokens.color.main_green_10 : tokens.color.grey_40_light};
+                font-weight: ${focus === index ? 600 : 500};
+                font-family: "Pretendard Variable", Pretendard, -apple-system,
+                  BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue",
+                  "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR",
+                  "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji",
+                  "Segoe UI Symbol", sans-serif;
+                margin: 0;
+                line-height: 1.25;
+                text-decoration: none;
+                @font-face {
+                  font-family: "Pretendard Variable", Pretendard, -apple-system,
+                    BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue",
+                    "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR",
+                    "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji",
+                    "Segoe UI Symbol", sans-serif;
+                  src: url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable.css");
+                }
+              `,
+      href: list2.link
+    }, list2.text), /* @__PURE__ */ createElement("div", {
+      className: import_css.css`
+                width: 3px;
+                background-color: ${tokens.color.main_green_10};
+                border-radius: 0px 2px 2px 0px;
+                color: ${tokens.color.main_green_10};
+                opacity: ${focus === index ? 1 : 0};
+                transition: all 0.2s cubic-bezier(0.215, 0.61, 0.355, 1);
+              `
+    }, "."));
+  }));
 }
-var sizeNormal = import_css.css``;
-var sizeSmall = import_css.css``;
+var contaier = import_css.css`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  gap: 20px;
+  align-items: start;
+  overflow: hidden;
+`;
+var itemContainer = import_css.css`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+`;
 export {
-  Button
+  SideNavigation
 };
