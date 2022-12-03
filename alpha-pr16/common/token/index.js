ea0a1ed1194188b94e5c65c7880f56ddd3a7945b
deploy <deploy@users.noreply.github.com>
2022-12-03 12:21:29 +0000

    Updated Alpha alpha-pr16

---
+++ alpha-pr16/common/token/index.js
@@ -0,0 +1,8 @@
+// src/common/token/index.ts
+import { color } from "./color.js";
+var token = {
+  color
+};
+export {
+  token
+};
---
+++ alpha-pr16/typography/text/utils/getFontWeight.js
@@ -0,0 +1,5 @@
+// src/typography/text/utils/getFontWeight.ts
+var getFontWeight = (size) => size === "header1" ? 600 : size === "header2" ? 600 : size === "header3" ? 600 : size === "header4" ? 600 : size === "body1" ? 500 : 500;
+export {
+  getFontWeight
+};
