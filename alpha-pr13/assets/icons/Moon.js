00667e407c7903edb579af8059bfacf52343d5d9
deploy <deploy@users.noreply.github.com>
2022-12-05 02:07:02 +0000

    Updated Alpha alpha-pr13

---
+++ alpha-pr13/assets/icons/Moon.js
@@ -0,0 +1,19 @@
+// src/assets/icons/Moon.tsx
+import {
+  createElement
+} from "react";
+var Moon = ({ color }) => {
+  return /* @__PURE__ */ createElement("svg", {
+    viewBox: "0 0 20 20",
+    fill: "currentColor",
+    width: "24",
+    height: "24",
+    color
+  }, /* @__PURE__ */ createElement("path", {
+    d: "M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
+  }));
+};
+var Moon_default = Moon;
+export {
+  Moon_default as default
+};
