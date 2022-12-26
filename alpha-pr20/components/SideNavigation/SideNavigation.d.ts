cf8963c24eb7fa545dd88c05f3fe944a389bdaba
deploy <deploy@users.noreply.github.com>
2022-12-26 15:23:26 +0000

    Reset Alpha alpha-pr20

--- alpha-pr20/components/SideNavigation/SideNavigation.d.ts
+++ alpha-pr20/components/SideNavigation/SideNavigation.d.ts
@@ -1,16 +1,15 @@
 /// <reference types="react" />
 import { Theme } from '../../common/type';
-import { Icons } from '../../common/type/icons';
 type ListProps = {
-    icon: Icons;
+    icon: string;
     text: string;
     link: string;
 };
 type Props = {
     theme: Theme;
-    currentLink: string;
+    focus: number;
     hasIcon: boolean;
     list: ListProps[];
 };
-export declare function SideNavigation({ theme, hasIcon, currentLink, list }: Props): JSX.Element;
+export declare function SideNavigation({ theme, hasIcon, list, focus }: Props): JSX.Element;
 export {};
